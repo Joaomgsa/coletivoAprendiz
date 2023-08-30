e937c016ed080e5e0efc10e438ab01bef2525af5
joaomgsa <joaomgsa@gmail.com>
2023-08-30 11:08:53 -0300

    adicionando arquivo de saída

--- presença.py
+++ presença.py
@@ -102,3 +102,4 @@ alunos.to_excel('alunos.xlsx', index=False)
 #       2 - Futuramente colocar um laço de repetição para verificar até a ultima planilha da pasta
 #       3 - Criar um filtro para verificar se a coluna é NaN e remover a coluna de uma forma recursiva
 #       4 - Fazer a leitura da data da aula
+#       5 - Diminuir as gambiarras
